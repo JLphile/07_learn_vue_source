32d8841986b687cf9f6200e5b66907d0a2960830
JLphile <lkyz001@outlook.com>
2022-09-15 20:36:37 +0800

    render-mount-patch

--- 02_渲染器实现/render.js
+++ 02_渲染器实现/render.js
@@ -74,5 +74,45 @@ const patch = (n1, n2) => {
       }
     }
     // 3.处理children
+    const oldChildren = n1.children || [];
+    const newChildren = n2.children || [];
+    // 边界判断
+    if (typeof newChildren === 'string') {
+      //情况一：newChildren本身是一个string
+      if (newChildren !== oldChildren) {
+        el.textContent = newChildren;
+      } else {
+        el.innerHTML = newChildren;
+      }
+    } else {
+      //情况二：newChildren本身是一个数组
+      if (typeof oldChildren === 'string') {
+        el.innerHTML = '';
+        newChildren.forEach((item) => {
+          mount(item, el);
+        });
+      } else {
+        // oldChildren:[v1,v2,v3]
+        // newChildren:[v1,v5,v6,v7,v8]
+        // diff算法：如果newChildren更长，那么diff结束后进行添加操作，如果oldChildren更长，diff结束后进行删除操作
+        // 1.前面有相同节点的原生进行patch操作
+        const commonLength = Math.min(oldChildren.length, newChildren.length);
+        for (let i = 0; i < commonLength; i++) {
+          patch(oldChildren[i], newChildren[i]);
+        }
+        // 2.如果newChildren.length>oldChildren.length，那么diff结束后进行添加操作
+        if (newChildren.length > oldChildren.length) {
+          newChildren.slice(oldChildren.length).forEach((item) => {
+            mount(item, el);
+          });
+        }
+        // 3.newChildren.length < oldChildren.length,diff结束后进行删除操作
+        if (newChildren.length < oldChildren.length) {
+          oldChildren.slice(newChildren.length).forEach((item) => {
+            el.removeChild(item.el);
+          });
+        }
+      }
+    }
   }
 };
